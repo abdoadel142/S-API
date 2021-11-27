6f44a77bcbd2d859c98dc40969af1f2c20a5fa33
Abdelrhman Adel 142 <abdoadel887@gmail.com>
2021-11-27 15:49:47 +0200

    validation update

--- src/main/java/com/example/SAPI/student/StudentService.java
+++ src/main/java/com/example/SAPI/student/StudentService.java
@@ -2,9 +2,11 @@ package com.example.SAPI.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,26 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
 
+    }
+
+     @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        boolean exists = studentRepository.existsById(studentId);
+        Student student = studentRepository.getById(studentId);
+
+        if (!exists){
+            new IllegalStateException(("student with Id" +studentId+ "does not exist"));
+
+        }
+        if(name!= null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email!= null && email.length()>0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
